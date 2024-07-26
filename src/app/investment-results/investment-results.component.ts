300fe79eec2cdd74650e574a1e9176a50cf46933
Roman Milata <roman.milata.za@gmail.comgit config --global user.email roman.milata.za@gmail.comgit config --global user.email roman.milata.za@gmail.com>
2024-07-26 14:01:42 +0200

    investment.service.ts; investment-results.comp.html, ts: changing to signals - acc.to: 87. Using Signals in Services

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -7,12 +7,11 @@ import { InvestmentService } from '../investment.service';
   standalone: true,
   imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultData;
-  }
+  // results = this.investmentService.resultData.asReadonly();
+  results = computed(() => this.investmentService.resultData());
 }
--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 import type{ InvestmentInput } from "./investment-input-model";
 
 @Injectable({providedIn: 'root'})
 export class InvestmentService {
-    resultData?: {   
+    resultData = signal <{   
         year: number;
         interest: number;
         valueEndOfYear: number;
         annualInvestment: number;
         totalInterest: number;
         totalAmountInvested: number;
-      } [] 
+      } [] | undefined>(undefined);
 
     calculateInvestmentResults(data: InvestmentInput) {
         const { initialInvestment, annualInvestment, expectedReturn, duration } =
@@ -35,7 +35,6 @@ export class InvestmentService {
         }
     
         // return annualData;
-        this.resultData = annualData;
-      }
-
+        this.resultData.set(annualData);
+    }
 }
